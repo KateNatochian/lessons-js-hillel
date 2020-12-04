907c380fcce917743fe50c6e3a18498d8baf3293
Katerine Natochina <72230900+KateNatochian@users.noreply.github.com>
2020-12-04 21:57:09 +0200

    Update main.js

--- lesson#3_3/main.js
+++ lesson#3_3/main.js
@@ -1,12 +1,8 @@
 A = new Array(1, 2, 3, 4, 5, 6);
 
-for (i = 0; i < A.length - 1; i++) {
-  for (j = 0; j < A.length - i - 1; j++) {
-    k = A[j];
-    A[j] = A[j + 1];
-    A[j + 1] = k;
-  }
+for (i = 0; i < A.length/2; i++) {
+  additional = A[i];
+  A[i] = A[A.length - i-1];
+  A[A.length - i-1] = additional;
 }
 console.log(A);
-
-

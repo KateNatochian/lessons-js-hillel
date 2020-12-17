8d6302274d1ce7038668af9db9a3de4029fcfb63
Katerine Natochina <72230900+KateNatochian@users.noreply.github.com>
2020-12-17 23:07:38 +0200

    Update main.js

--- lesson#6_2/main.js
+++ lesson#6_2/main.js
@@ -7,24 +7,24 @@ B = fillArray(B, 10);
 // console.log(A);
 // console.log(B);
 
-// console.log(sumArray(A));
-// console.log(sumArray(B));
 console.log(biggerArray(A, B));
 
 function biggerArray(Array1, Array2) {
+  
+  function sumArray(Array) {
+    let sum = 0;
+    for (let i = 0; i < Array.length; i++) {
+      sum = sum + Array[i];
+    } 
+    return sum;
+  }
+
   if (sumArray(Array1) > sumArray(Array2)) {
     return Array1;
   }
   return Array2;
 }
 
-function sumArray(Array) {
-  let sum = 0;
-  for (let i = 0; i < Array.length; i++) {
-    sum = sum + Array[i];
-  } 
-  return sum;
-}
 
 function fillArray(A, a) {  
   let B = new Array();

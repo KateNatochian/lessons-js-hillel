20123236f2c7ae50ddd6cef5e414b0f7e50173bf
Katerine Natochina <72230900+KateNatochian@users.noreply.github.com>
2020-12-25 23:25:08 +0200

    Add files via upload

---
+++ lesson#8_1/main.js
@@ -0,0 +1,25 @@
+let A = [34, 17, 45, 65];
+let B = [43, 33, 56, 18];
+let D = [38, 66, 89, 23];
+
+function getMaxs(A, B, D) {
+  let maxArray = [];
+  function max(array) {
+    if (Array.isArray(array)) {
+      let maxValue = array[0];
+      for (let i = 0; i < array.length; i++) {
+        if (array[i] > maxValue) {
+          maxValue = array[i];
+        }
+      }
+      return maxValue;
+    }
+  }
+  let arrArrs = [A, B, D];
+  for (let i = 0; i < arrArrs.length; i++) {
+    maxArray.push(max(arrArrs[i]));
+  }
+
+  return maxArray;
+}
+console.log(getMaxs(A, B, D));

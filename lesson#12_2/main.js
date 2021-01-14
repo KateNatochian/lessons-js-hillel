e7a6eff2fa9de1889bc34f1f7cf2f2b21b7cbdaa
Katerine Natochina <72230900+KateNatochian@users.noreply.github.com>
2021-01-14 12:58:55 +0200

    Add files via upload

---
+++ lesson#12_2/main.js
@@ -0,0 +1,50 @@
+window.addEventListener('load', function(){
+    const square = document.querySelector('.square');
+    let newLeft;
+
+    function eventHandler(event){
+        // console.log(event.type, event);
+        if(!isBlockInWindow()){   
+            return true;
+        }
+        
+        // move right
+        if(event.type == 'keydown' && event.keyCode == 39){
+            console.log(square.style.left)
+            if(square.style.left == ''){
+                newLeft = window.innerWidth/2;
+            } else{
+                newLeft = parseInt(square.style.left)+1;
+            }
+            square.style.left = `${newLeft+2}px`;
+            
+        }
+        // move left
+      if (event.type == 'keydown' && event.keyCode == 37) {
+        console.log(square.style.left)
+        if (square.style.left == '') {
+          newLeft = window.innerWidth / 2;
+        } else {
+          newLeft = parseInt(square.style.left) - 1;
+        }
+        square.style.left = `${newLeft - 2}px`;
+            
+        }  
+    }
+    document.addEventListener('keydown', eventHandler);
+
+    function isBlockInWindow(){
+        
+        //right side
+        if(square.offsetLeft + square.offsetWidth >= window.innerWidth){
+            square.style.left = window.innerWidth - square.offsetWidth - 2 + 'px';
+            return false;
+        }
+        // left side
+        if(square.offsetLeft <= 0){
+            square.style.left = 2 + 'px';
+            return false;
+        }
+        return true;
+    }
+});
